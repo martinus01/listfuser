e37dda167fbcfe662ad4487efc531b525b945a8c
martinus01 <63968610+martinus01@users.noreply.github.com>
2020-05-08 23:21:19 +0200

    initi

---
+++ app/src/main/java/com/example/listfuser/Locations.java
@@ -0,0 +1,6 @@
+package com.example.listfuser;
+
+class Locations {
+    private String name;
+
+}
--- app/src/main/java/com/example/listfuser/RestLaunchResponse.java
+++ app/src/main/java/com/example/listfuser/RestLaunchResponse.java
@@ -6,6 +6,7 @@ public class RestLaunchResponse {
     private List<Launch> launches;
     private Integer total;
     private Integer count;
+    private List<Locations> locations;
 
     public List<Launch> getLaunches() {
         return launches;
